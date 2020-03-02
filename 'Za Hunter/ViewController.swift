c2cacc164141d9131ae341961296b3b76298cfcf
Sydney Ripsky <sripsky@bsd220.org>
2020-03-02 10:52:49 -0600

    Zoom to current location

--- 'Za Hunter/ViewController.swift
+++ 'Za Hunter/ViewController.swift
@@ -10,19 +10,27 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
-let locationManager = CLLocationManager()
-    
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion ()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
-
-
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
+        region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 
 }
 
